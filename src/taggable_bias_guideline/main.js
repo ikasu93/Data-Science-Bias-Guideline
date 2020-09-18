2a9d79727462735ca8266edd1056e59cd285219f
ikasu93 <46449336+ikasu93@users.noreply.github.com>
2020-09-18 20:23:27 +0200

    Update main.js
    add autocompletion

--- src/taggable_bias_guideline/main.js
+++ src/taggable_bias_guideline/main.js
@@ -146,12 +146,21 @@ grid`);
               }, 'Create Tag Summary', 'Default tags')
           ])
       }
+	  var autocompleteButton = function () {
+          Jupyter.toolbar.add_buttons_group([	
+              Jupyter.keyboard_manager.actions.register ({
+                  'help': 'Autocomplete',
+                  'icon' : 'magic',
+                  'handler': activateAutocomplete
+              }, 'Autocomplete', 'Default autocomplete')
+          ])
+      }
     // Run on start
     function load_ipython_extension() {
         defaultCellButton();
 		activateTagsButton();
 		tagCellButton();
-		
+		autocompleteButton();
     }
     return {
         load_ipython_extension: load_ipython_extension
@@ -180,6 +189,7 @@ var add_cell = function() {
 var boolTag = false;
 function activateTags(){
 	if (boolTag === false){
+		
 		document.querySelector("#menu-cell-toolbar-submenu > li:nth-child(6) > a").click();
 		boolTag = true;
 	}
@@ -190,6 +200,7 @@ function activateTags(){
 }
 
 function tags(){
+
 	const tagList = [];
 	
 	Jupyter.notebook.get_cells().forEach(cell => {
@@ -214,6 +225,95 @@ function tags(){
 		window.alert("No taggings found!")
 	}
 	var tString = tagList.toString();
-	
+
 }
 
+/*
+// Autocompletion for Tagging
+*/
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+	  a.style.borderBottom = "1px solid #d4d4d4"; 
+	  a.style.zIndex = "5";
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+		  
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+
+  });
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the keywords*/
+var keywords = ["databias", "representationbias", "repair", "labeling", "datatargeting", "advanceddatamining", "selectionbias", "randomsubgroupchoice",
+"samplingbias", "simplerandomsampling", "stratifiedrandomsampling",
+"measurementbias", "systematicerrors", "multiplemeasuringdevices", "multipledataspecialists",
+"surveybias", "selfreportingbias", "balancedquestionnaires",
+"seasonalbias", "indepthseasonaltrendsknowledge", "historicaltrendreview",
+"survivorshipbias",
+"historicalbias", "knearestneigbor", "arima", "regression", "angelbasedoutlierdetection",
+"dataenrichmentbias", "differentrankingparameters", "kernalbasedlocaloutlierfactor",
+"cognitivebias", "datavisualization", "framingeffect", "strategicvisualizationtools",
+"avaiabilitybias", "properweighting",
+"overconfidencebias", "scientificmindset",
+"anchoringbias", "criticalthinking", "decisionsviahistoricalpatterns",
+"confirmationbias", "signalerror", "alternativesources", "testing",
+"algorithmicbias", "aif360", "fairecsys",
+"hothandfallacy", "systematicthinking",
+"bandwagoneffect", "rethinking",
+"groupattributionbias", "ingroupbias", "outgrouphomogeneitybias", "selfanalysis",
+"aggregationbias", "coupledlearningtechniques", "multitasklearning", "fairrepresentationlearning", "spaceprojection", 
+"evaluationbias", "subgroupevaluation", "multiplemetrics", "confidenceintervals", "smote",
+"deploymentbias", "usertraining", "ethicalmodeltrainingwithunbiaseddata",
+"rescuebias", "selfevaluation",
+"overfitting", "underfitting", "processmining", "penaltymethods", "earlystopping", "L14regulation", "preprocessing", "postprocessing"];
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+
+function activateAutocomplete(){
+	$("input").on("click", () => autocomplete($("input:focus").prevObject[0].activeElement, keywords));
+}
